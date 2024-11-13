15615d6f0c1940d6a1042026416edceb8e20dfaf
Ph0Byte <kartonjuan4@gmail.com>
2024-11-12 19:47:33 -0500

    Delete src/main/java/inicio/proyectoreserva/hilos directory

--- src/main/java/inicio/proyectoreserva/hilos/Tarea.java
+++
@@ -1,8 +0,0 @@
-package inicio.proyectoreserva.hilos;
-
-public class Tarea implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("Hilo en ejecucion: " + Thread.currentThread().getName());
-    }
-}
